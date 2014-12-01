f6d81ce74ba963c004b6e78fedeeef0a9c1fc50d
MaRuoze <51899608@qq.com>
2014-12-01 15:33:05 +0800

    1.会员功能的权限验证完成。 2.httpClient与webView共享cookie失败，只能使用POST登录验证方法。

--- JYJ_Android_Git/src/com/jxtzw/app/ui/MemberPrivilegeActivity.java
+++ JYJ_Android_Git/src/com/jxtzw/app/ui/MemberPrivilegeActivity.java
@@ -1,11 +1,27 @@
 package com.jxtzw.app.ui;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.params.HttpParams;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 
 import net.tsz.afinal.FinalHttp;
@@ -23,10 +39,13 @@ import com.jxtzw.app.common.UIHelper;
 import com.jxtzw.app.view.WebMemberView;
 
 import android.R.bool;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -60,31 +79,24 @@ public class MemberPrivilegeActivity extends BaseActivity {
 	 * WebView
 	 */
 	protected WebMemberView mWebMemberView;
-	/**
-	 * SharedPreferences
-	 */
-	private SharedPreferences mSharedPreferences;
-	/**
-	 * 登录验证数据
-	 */
-	private String mUsernameString;
-	private String mPasswordString;
-	
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_member_privilege);
 		init();
 	}
-	
+
 	/**
 	 * 调用父类构造函数供子类使用
+	 * 
 	 * @param savedInstanceState
 	 */
 	protected void onCreateEx(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
-	
+
 	/**
 	 * 初始化
 	 */
@@ -93,126 +105,130 @@ public class MemberPrivilegeActivity extends BaseActivity {
 		initMemberVar();
 		initSelfMemVar();
 		initTitle();
-		initCookie();
+		//由于同步httpclient和webview的cookie出现问题，所以initCookie被注释
+		//initCookie();
 		initWebView();
 	}
-	
+
 	/**
 	 * 初始化通用成员变量
 	 */
 	protected void initCommonMemberVar() {
-		//上下文
-		mContext=this;
-		mActivity=(Activity)mContext;
-		mAppContext=(AppContext) mActivity.getApplication();
-		//资源
-		mResources=getResources();
-		//布局
-		mLayoutInflater=LayoutInflater.from(this);
+		// 上下文
+		mContext = this;
+		mActivity = (Activity) mContext;
+		mAppContext = (AppContext) mActivity.getApplication();
+		// 资源
+		mResources = getResources();
+		// 布局
+		mLayoutInflater = LayoutInflater.from(this);
 	}
-	
+
 	/**
 	 * 初始化成员变量
 	 */
 	protected void initMemberVar() {
-		//获取Intent
-		Intent intent=getIntent();
-		Bundle bundle=intent.getExtras();
-		mMemberIndex=bundle.getInt("MemberIndex");
-		mMainTitle=bundle.getString("MainTitle");
+		// 获取Intent
+		Intent intent = getIntent();
+		Bundle bundle = intent.getExtras();
+		mMemberIndex = bundle.getInt("MemberIndex");
+		mMainTitle = bundle.getString("MainTitle");
 	}
-	
+
 	/**
 	 * 初始化本类成员变量
 	 */
 	protected void initSelfMemVar() {
-		mSubTitles=mResources.getStringArray(R.array.member_title);
-		mCatName=mSubTitles[mMemberIndex];
-		mMemberAPIs=mResources.getStringArray(R.array.member_api);
-		mMemberAPI=mMemberAPIs[mMemberIndex];
+		mSubTitles = mResources.getStringArray(R.array.member_title);
+		mCatName = mSubTitles[mMemberIndex];
+		mMemberAPIs = mResources.getStringArray(R.array.member_api);
+		mMemberAPI = mMemberAPIs[mMemberIndex];
 	}
-	
+
 	/**
 	 * 标题栏初始化
 	 */
 	protected void initTitle() {
-		mHeadLeft=(Button) findViewById(R.id.head_left);
-		mHeadRight=(Button) findViewById(R.id.head_right);
-		mCatNameTextView=(TextView) findViewById(R.id.head_middle);
-		String titleString=mMainTitle+" • "+mCatName;
+		mHeadLeft = (Button) findViewById(R.id.head_left);
+		mHeadRight = (Button) findViewById(R.id.head_right);
+		mCatNameTextView = (TextView) findViewById(R.id.head_middle);
+		String titleString = mMainTitle + " • " + mCatName;
 		mCatNameTextView.setText(titleString);
-		//初始化监听
+		// 初始化监听
 		initTilteListner();
 		mHeadLeft.setOnClickListener(mHLClickListener);
 		mHeadRight.setOnClickListener(mHRClickListener);
 	}
-	
+
 	/**
 	 * 标题栏事件监听初始化
 	 */
-	protected void initTilteListner(){
-		mHLClickListener=new OnClickListener() {
+	protected void initTilteListner() {
+		mHLClickListener = new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				finish();
 			}
 		};
-		mHRClickListener=new OnClickListener() {
+		mHRClickListener = new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				UIHelper.showMainPopMenu(mContext, R.layout.pop_users, mHeadRight);
+				UIHelper.showMainPopMenu(mContext, R.layout.pop_users,
+						mHeadRight);
 			}
 		};
 	}
-	
-	
+
 	/**
 	 * WebView初始化
 	 */
 	protected void initWebView() {
-		mWebMemberView=new WebMemberView(mContext);
-		View view=((ViewGroup)findViewById(android.R.id.content)).getChildAt(0);
+		mWebMemberView = new WebMemberView(mContext);
+		View view = ((ViewGroup) findViewById(android.R.id.content))
+				.getChildAt(0);
 		mWebMemberView.init(view, mMemberAPI, mMemberIndex);
-		//mWebMemberView.update();
+		mWebMemberView.update();
 	}
-	
-	
+
 	/**
 	 * 登录并设置session
 	 */
-	private void initCookie(){
-		mSharedPreferences=AppConfig.getSharedPreferences(mContext);
-		mUsernameString=mSharedPreferences.getString("username", "");
-		String encryPassword=mSharedPreferences.getString("password", "");
-		if(mUsernameString.length()==0||encryPassword.length()==0){
+	/**private void initCookie() {
+		mSharedPreferences = AppConfig.getSharedPreferences(mContext);
+		mUsernameString = mSharedPreferences.getString("username", "");
+		String encryPassword = mSharedPreferences.getString("password", "");
+		if (mUsernameString.length() == 0 || encryPassword.length() == 0) {
 			UIHelper.ToastMessage(mContext, "登录信息有误，请重新登录！");
-		}else{
-			mPasswordString=CyptoUtils.decode("maru09743233aoze", encryPassword);
-			if (mPasswordString==null) {
+		} else {
+			mPasswordString = CyptoUtils.decode("maru09743233aoze",
+					encryPassword);
+			if (mPasswordString == null) {
 				UIHelper.ToastMessage(mContext, "登录信息有误，请重新登录！[密码]");
-			}else{
-				//判断是否有可用网络
+			} else {
+				// 判断是否有可用网络
 				if (!mAppContext.isNetworkConnected()) {
-					//如果无可用网络则直接返回
-					UIHelper.ToastMessage(mContext, R.string.network_not_connected);
+					// 如果无可用网络则直接返回
+					UIHelper.ToastMessage(mContext,
+							R.string.network_not_connected);
 					return;
 				}
 				loginProcess();
+				//loginProcessHttp();
 			}
 		}
-	}
-	
+	}*/
+
 	/**
 	 * 登录过程
 	 */
-	private void loginProcess() {
-		String apiLoginCookie=mResources.getString(R.string.api_login_cookie);
-		AjaxParams ajaxParams=new AjaxParams();
+	/**private void loginProcess() {
+		String apiLoginCookie = mResources.getString(R.string.api_login_cookie);
+		AjaxParams ajaxParams = new AjaxParams();
 		ajaxParams.put("username", mUsernameString);
 		ajaxParams.put("password", mPasswordString);
-		final FinalHttp finalHttp=new FinalHttp();
+		final FinalHttp finalHttp = new FinalHttp();
 		finalHttp.post(apiLoginCookie, ajaxParams, new AjaxCallBack<String>() {
 
 			@Override
@@ -225,17 +241,85 @@ public class MemberPrivilegeActivity extends BaseActivity {
 			@Override
 			public void onSuccess(String t) {
 				// TODO Auto-generated method stub
-				DefaultHttpClient client=(DefaultHttpClient) finalHttp.getHttpClient();
-				//HttpContext httpContext=finalHttp.getHttpContext();
-				List<Cookie> cookies=client.getCookieStore().getCookies();
+				DefaultHttpClient client = (DefaultHttpClient) finalHttp
+						.getHttpClient();
+				// HttpContext httpContext=finalHttp.getHttpContext();
+				List<Cookie> cookies = client.getCookieStore().getCookies();
 				for (int i = 0; i < cookies.size(); i++) {
-					Cookie cookie=cookies.get(i);
+					Cookie cookie = cookies.get(i);
 					if (cookie.getName().equalsIgnoreCase("ci_session")) {
-						AppConfig.COOKIE=cookie;
+						AppConfig.COOKIE = cookie;
 					}
 				}
 				mWebMemberView.update();
 			}
 		});
-	}
+	}**/
+
+	/**
+	 * 登录过程httpclient
+	 */
+	/**@SuppressLint("HandlerLeak")
+	private void loginProcessHttp() {
+		final Handler handler = new Handler() {
+
+			@Override
+			public void handleMessage(Message msg) {
+				// TODO Auto-generated method stub
+				if (msg.what==0) {
+					mWebMemberView.update();
+				}
+			}
+
+		};
+
+		Thread thread = new Thread() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				String apiLoginCookie = mResources.getString(R.string.api_login_cookie);
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpPost httpPost = new HttpPost(apiLoginCookie);
+				ArrayList<NameValuePair> nvps = new ArrayList<NameValuePair>();  
+		        nvps.add(new BasicNameValuePair("username", mUsernameString));  
+		        nvps.add(new BasicNameValuePair("password", mPasswordString));  
+		        try {
+					httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+				} catch (UnsupportedEncodingException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}  
+				HttpContext context = new BasicHttpContext();
+				CookieStore cookieStore = new BasicCookieStore();
+				context.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+				HttpResponse response = null;
+				try {
+					response = client.execute(httpPost, context);
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+					// 根据你的逻辑，判断返回的值是不是表示已经登录成功
+					List<Cookie> cookies = cookieStore.getCookies();
+					if (!cookies.isEmpty()) {
+						for (int i = cookies.size(); i > 0; i--) {
+							Cookie cookie = cookies.get(i - 1);
+							if (cookie.getName().equalsIgnoreCase("ci_session")) {
+								// 使用一个常量来保存这个cookie，用于做session共享之用
+								AppConfig.COOKIE = cookie;
+								handler.sendEmptyMessage(0);
+							}
+						}
+					}
+				}
+			}
+
+		};
+		thread.start();
+	}**/
 }
--- JYJ_Android_Git/src/com/jxtzw/app/view/WebMemberView.java
+++ JYJ_Android_Git/src/com/jxtzw/app/view/WebMemberView.java
@@ -1,25 +1,29 @@
 package com.jxtzw.app.view;
 
-import java.net.URLDecoder;
-import java.util.Hashtable;
-
-import org.apache.http.cookie.Cookie;
-
 import com.jxtzw.app.AppConfig;
 import com.jxtzw.app.AppContext;
 import com.jxtzw.app.R;
+import com.jxtzw.app.common.CyptoUtils;
 import com.jxtzw.app.common.UIHelper;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
-import android.webkit.CookieManager;
-import android.webkit.CookieSyncManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class WebMemberView extends WebQuoView {
-
+	/**
+	 * SharedPreferences
+	 */
+	private SharedPreferences mSharedPreferences;
+	/**
+	 * 登录验证数据
+	 */
+	private String mUsernameString;
+	private String mPasswordString;
 	public WebMemberView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
@@ -52,10 +56,10 @@ public class WebMemberView extends WebQuoView {
 				
 		//页面加载
 		mWebView=(WebView) mListView.findViewById(R.id.webview_quotation);
-		//mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.getSettings().setJavaScriptEnabled(true);
 		//mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 		//WebChromeClient主要辅助WebView处理Javascript的对话框、网站图标、网站title、加载进度
-		//mWebView.setWebChromeClient(new WebChromeClient());
+		mWebView.setWebChromeClient(new WebChromeClient());
 		//WebViewClient处理基本页面效果
 		mWebView.setWebViewClient(new WebViewClient(){
 			@Override
@@ -65,32 +69,42 @@ public class WebMemberView extends WebQuoView {
 				mLoadedView.setVisibility(View.GONE);
 			}
 		});
+		mSharedPreferences = AppConfig.getSharedPreferences(mContext);
+		mUsernameString = mSharedPreferences.getString("username", "");
+		String encryPassword = mSharedPreferences.getString("password", "");
+		if (mUsernameString.length() == 0 || encryPassword.length() == 0) {
+			UIHelper.ToastMessage(mContext, "登录信息有误，请重新登录！");
+		} else {
+			mPasswordString = CyptoUtils.decode("maru09743233aoze",
+					encryPassword);
+			if (mPasswordString == null) {
+				UIHelper.ToastMessage(mContext, "登录信息有误，请重新登录！[密码]");
+			} else {
+				String postData="username="+mUsernameString+"&&password="+mPasswordString;
+				mWebView.postUrl(mCatID, postData.getBytes());
+			}
+		}
+		
 		
 		
 		//cookie同步
-		String apiLoginCookie=mResources.getString(R.string.api_login_cookie);
+		/**String apiLoginCookie=mResources.getString(R.string.api_login_cookie);
 		CookieSyncManager.createInstance(mContext);
 		CookieManager cookieManager=CookieManager.getInstance();
 		Cookie sessionCookie=AppConfig.COOKIE;
 		if (sessionCookie!=null) {
-			//String cookieString = sessionCookie.getName() + "=" + URLDecoder.decode(sessionCookie.getValue()) + "; domain=" + sessionCookie.getDomain();
-			//String cookieString = sessionCookie.getName() + "=" + sessionCookie.getValue() + "; domain=" + sessionCookie.getDomain();
-			String cookieString=sessionCookie.toString();
+			//String cookieString = sessionCookie.getName() + "=" + URLDecoder.decode(sessionCookie.getValue()) + "; domain=" + sessionCookie.getDomain()+";path="+sessionCookie.getPath();;
+			String cookieString = sessionCookie.getName() + "=" + sessionCookie.getValue() + "; domain=" + sessionCookie.getDomain()+";path="+sessionCookie.getPath();
+			//String cookieString=sessionCookie.toString();
 			cookieManager.setAcceptCookie(true);
 			cookieManager.removeSessionCookie();//移除
 			CookieSyncManager.getInstance().sync();
-			cookieManager.setCookie("/", cookieString);
+			cookieManager.setCookie(sessionCookie.getDomain(), cookieString);
 			CookieSyncManager.getInstance().sync();
-			String cookie=cookieManager.getCookie("/");
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			String cookie=cookieManager.getCookie(sessionCookie.getDomain());
 			if(cookie!=null){
 				mWebView.loadUrl(mCatID);
 			}
-		}
+		}**/
 	}
 }
